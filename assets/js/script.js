f19e8278fdd3d6f90df92ef0f0d2cd25fcaf3b79
vcaruso <vinnycar0923@gmail.com>
2020-06-18 19:12:34 -0700

    can't figure out how to change class based on time!

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,25 @@
 // Display Date at Top
 var dateToday = moment().format('MMMM Do YYYY');
 $("#currentDay").append(dateToday);
+
+//Change textarea background color based on time
+var checkTime = function () {
+    var hour = $(".hour").text().trim();
+
+    var time = moment(hour, "LT");
+    console.log(time)
+
+    //remove any old classes from element
+    $(".hour").removeClass(".present .past .future");
+
+    // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+        $(".hour").addClass(".past");
+    } else if (moment().isBefore(time)) {
+        $(".hour").addClass(".future");
+    } else {
+        $(".hour").addClass(".present");
+    }
+}
+
+checkTime();
